d152a73ee82675cf3b3467fe7d2aa2a2fa6efdd0
Dirk Gadsden <dirk@esherido.com>
2015-04-18 15:50:07 -0400

    Refactor YAML data slicing

--- lib/fixtural/downloader.rb
+++ lib/fixtural/downloader.rb
@@ -24,8 +24,7 @@ module Fixtural
       else
         tables = @adapter.guess_tables
       end
-      tables = tables.slice 0, 10
-      
+
       total = tables.length
       tables.each_with_index do |table, index|
         path = File.join(@download_directory, "#{table}.yml")
@@ -42,13 +41,25 @@ module Fixtural
       def initialize io
         @io = io
         @io.write "---\n"
+        @checked = false
       end
       def write object
-        # Convert the index-object to YAML and strip off the document start and end tag
+        # Convert the index-object to YAML
         data = object.to_yaml
-        data.sub!(/^---\n/, '')
-        data.sub!(/\.\.\.\n$/, '')
+        # Check conformance of the objects if we haven't already
+        if !@checked
+          detect_slicing_offsets data
+          @checked = true
+        end
+        # Figure out which parts we need to chop off
+        data = data.slice(@head_offset, data.length - @tail_offset)
         @io.write data
+      end
+      def detect_slicing_offsets data
+        @strip_head = (data =~ /^---\n/) ? true : false
+        @strip_tail = (data =~ /\.\.\.\n$/) ? true: false
+        @head_offset = @strip_head ? 4 : 0
+        @tail_offset = (@strip_tail ? 4 : 0) + @head_offset
       end
       def done
       end
